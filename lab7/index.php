4aaadd994355c476d5c83a65a608070a6cb0cc9e
66112574-tltle <66112574@dpu.ac.th>
2024-11-14 12:38:08 +0700

    11/14

--- lab7/index.php
+++ lab7/index.php
@@ -1,16 +1,12 @@
 <?php
-$int1 = 28;    # => 28
-$int2 = -32;   # => -32
-$int3 = 012;   # => 10 (octal)
-$int4 = 0x0F;  # => 15 (hex)
-$int5 = 0b101; # => 5  (binary)
-$int6 = 2_000_100_000;
-
-echo "$int1<br>";
-echo "$int2<br>";
-echo "$int3<br>";
-echo "$int4<br>";
-echo "$int5<br>";
-echo "$int6<br>";
-echo 1 + "-1.3e3";
+include 'header.php';
+?>
+      <h1>Welcome</h1>
+    <?php
+    echo $_SESSION['user']['firstName'].' '.
+         $_SESSION['user']['lastName'];
+?>
+</h1>
+<?php
+include 'footer.php';
 ?>
